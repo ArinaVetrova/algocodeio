857790982d1902aa4e869150a1bb122599ecb850
Arina Vetrova <renovetrova@gmail.com>
2026-02-23 15:21:39 +0300

    Change LRU cache for O(1) operations
    Use list + unordered_map structs instead of previous version

--- LRUCache/TLruCache.h
+++ LRUCache/TLruCache.h
@@ -1,7 +1,7 @@
 #pragma once 
 
 #include <unordered_map>
-#include <set>
+#include <list>
 #include <iostream>
 
 using namespace std;
@@ -10,35 +10,19 @@ using namespace std;
 template <typename Key, typename Val>
 class TLruCache {
 private:
-    // used Timestamp alias, used uint64_t instead of signed int
-    // (time should be always positive, signed int can overflow)
-    using Timestamp = uint64_t;
     // size_t matches container size() type and avoids signed/unsigned bugs
-    size_t SizeLimit = 0;
 
-    Timestamp Tm = 0;
+    size_t SizeLimit = 0;
+    struct Node
+    {
+        Key key;
+        Val val;
+    };
 
     // rewrite containers using template params and alias
-    unordered_map<Key, Val> Data;
-    unordered_map<Key, Timestamp> LastUsed;
-    set<pair<Timestamp, Key>> Queue;
-
-    // added helper method ResetKeyUsage
-    void ResetKeyUsage(const Key& key) {
-        auto it = LastUsed.find(key);
-        if (it == LastUsed.end()){
-            return;
-        }
-        Queue.erase({it->second, key});
-        LastUsed.erase(key);
-    }
-
-    // added helper method UpdateKeyUsage
-    void UpdateKeyUsage(const Key& key) {
-        Queue.erase({LastUsed[key], key});
-        LastUsed[key] = Tm++;
-        Queue.insert({LastUsed[key], key});
-    }
+    // move to list + unordered_map to provide O(1) for cache operations
+    std::list<Node> CacheList;
+    std::unordered_map<Key, typename std::list<Node>::iterator> Data;
 
 public:
     TLruCache(size_t sizeLimit) : SizeLimit(sizeLimit) {
@@ -50,17 +34,17 @@ public:
     // changed exception throw to false return, added log for error case
     // used helper func to simplify
     bool Erase(const Key& key) {
-        if (!Data.count(key)) {
+        auto listIt = Data.find(key);
+        if (listIt == Data.end()) {
             std::cerr << "Erase: key not found" << std::endl;
             return false;
         }
 
-        ResetKeyUsage(key);
+        CacheList.erase(listIt->second);
         Data.erase(key);
         return true;
     }
 
-    // avoid dereferencing Queue.begin() when cache capacity is 0
     // use Val&&, baecause Val objects can be heavy
     void Set(const Key& key, Val&& value) {
         if (SizeLimit == 0)
@@ -69,22 +53,24 @@ public:
             return;
         }
 
-        // if there was a key found - it was also erased from the map --> delete unnecessary erase from map
-        if (Data.count(key)) {
-            ResetKeyUsage(key);
+        
+        // if we already have this keey, just update it's val and pos
+        if (auto it = Data.find(key); it != Data.end())
+        {
+            it->second->val = std::move(value);
+            CacheList.splice(CacheList.begin(), CacheList, it->second);
+            return;
         }
 
-        if (Queue.size() == SizeLimit) {
-            auto [_, key] = *Queue.begin();
-            Erase(key);
+        if (Data.size() == SizeLimit) {
+            const auto& lastNode = CacheList.back();
+            Data.erase(lastNode.key);
+            CacheList.pop_back();
         }
-
-        // before we inserted new {Tm, key} pair to the Queue, 
-        // the previous pair stayed in the Queue --> now deleting previous pair before update
-        UpdateKeyUsage(key);
 
         // use std::move in case Val objects are heavy
-        Data[key] = std::move(value);
+        auto it = CacheList.insert(CacheList.begin(), {key, std::move(value)});
+        Data[key] = it;
     }
 
     // user need to check returned value and use it in case it's not nullptr
@@ -92,13 +78,16 @@ public:
     // 1. to avoid copy of heavy Val; 
     // 2. to give signal outside that the key is not found - return nullptr instead of throwing an exception
     Val* TryGet(const Key& key) {
-        if (!Data.count(key)) {
+        auto itMap = Data.find(key);
+        if (itMap == Data.end()) {
             std::cerr << "Get: key not found" << std::endl;
             return nullptr;
         }
 
-        UpdateKeyUsage(key);
-        return &Data[key];
+        auto itList = itMap->second;
+        Val* val = &(itList->val);
+        CacheList.splice(CacheList.begin(), CacheList, itList);
+        return val;
     }
 
     // added const to func signature
